f005be58f1de91112dbd0e2cfd89d801c3a5a378
andrei.lisa <andreilisa2001@gmail.com>
2021-12-15 15:55:49 +0300

    tables

---
+++ mybatis/src/main/resources/sql.sql
@@ -0,0 +1,37 @@
+CREATE TABLE users
+(
+    id        SERIAL not null
+        constraint users_pkey
+            primary key,
+    email     varchar(255),
+    enabled   boolean ,
+    locked    boolean ,
+    password  varchar(255),
+    user_role varchar(255),
+    username  varchar(255)
+);
+CREATE TABLE products
+(
+    id      SERIAL          not null
+        constraint products_pkey
+            primary key,
+    name    varchar(255)     not null,
+    price   double precision not null,
+    user_id bigint,
+    FOREIGN KEY (user_id) REFERENCES users(id)
+
+);
+CREATE TABLE rating(
+
+                       isliked   boolean,
+                       productid integer not null
+                           constraint rating_productid_fkey
+                               references products,
+                       userid    integer not null
+                           constraint rating_userid_fkey
+                               references users,
+                       id       SERIAL  not null,
+                       constraint rating_pkey
+                           primary key (userid, productid)
+
+);
